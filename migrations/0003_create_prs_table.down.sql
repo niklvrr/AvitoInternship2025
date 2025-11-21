0c80ddcb137ff12c048ef498223dab622b12d869
RomanNikolaev <arem379@mail.ru>
2025-11-22 01:37:42 +0300

    some fixes

--- migrations/0003_create_prs_table.down.sql
+++ migrations/0003_create_prs_table.down.sql
@@ -5,7 +5,6 @@ DROP TABLE IF EXISTS pr_reviewers;
 
 DROP INDEX IF EXISTS idx_prs_status;
 DROP INDEX IF EXISTS idx_prs_team;
-DROP INDEX IF EXISTS idx_prs_team;
-DROP INDEX IF EXISTS idx_prs_author
+DROP INDEX IF EXISTS idx_prs_author;
 
 DROP TABLE IF EXISTS prs;
