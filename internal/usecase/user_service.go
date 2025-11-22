bd162800dce7fbde05c6c00f7eab8d2933a438a2
RomanNikolaev <arem379@mail.ru>
2025-11-22 19:44:02 +0300

    set is active service method

--- internal/usecase/user_service.go
+++ internal/usecase/user_service.go
@@ -2,12 +2,20 @@ package usecase
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"github.com/google/uuid"
 	"github.com/niklvrr/AvitoInternship2025/internal/domain"
 	"github.com/niklvrr/AvitoInternship2025/internal/infrastructure/models/dto"
 	"github.com/niklvrr/AvitoInternship2025/internal/infrastructure/models/result"
 	"github.com/niklvrr/AvitoInternship2025/internal/transport/dto/request"
 	"github.com/niklvrr/AvitoInternship2025/internal/transport/dto/response"
 	"go.uber.org/zap"
+)
+
+var (
+	incorrectIdError = errors.New("incorrect user id")
+	setIsActiveError = errors.New("set user status error")
 )
 
 type UserRepository interface {
@@ -28,7 +36,26 @@ func NewUserService(repo UserRepository, log *zap.Logger) *UserService {
 }
 
 func (s *UserService) SetIsActive(ctx context.Context, req *request.SetIsActiveRequest) (*response.SetIsActiveResponse, error) {
+	userId, err := uuid.Parse(req.UserId)
+	if err != nil {
+		return nil, fmt.Errorf(`invalid user id: %w`, err)
+	}
 
+	dto := &dto.SetIsActiveDTO{
+		UserId:   userId,
+		IsActive: req.IsActive,
+	}
+
+	res, err := s.repo.SetIsActive(ctx, dto)
+	if err != nil {
+		return nil, fmt.Errorf(`set user status error: %w`, err)
+	}
+
+	//return &response.SetIsActiveResponse{
+	//	res.Id.String(),
+	//	res.Name,
+	//	res.IsActive,
+	//}, nil
 }
 
 func (s *UserService) GetReview(ctx context.Context, req *request.GetReviewRequest) (*response.GetReviewResponse, error) {
